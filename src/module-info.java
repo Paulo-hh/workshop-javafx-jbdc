d39d2b096b6ad7f75dfa7d9736f8522e54625e87
Paulo Hoff <paulohhoff@gmail.com>
2024-12-10 22:27:40 -0300

    Main view

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,8 @@
 module Workshop_javafx_jbdc {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	exports gui to javafx.fxml;  
+	opens gui to javafx.fxml;
 }
