fd75f79d5d8152085aee68ad2a50b11c08b03bc8
Paulo Hoff <paulohhoff@gmail.com>
2024-12-14 01:18:36 -0300

    Saving Seller

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -92,5 +92,14 @@ public class Utils {
 			}
 		});
 	}
+	
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+		}
+		catch(NumberFormatException e) {
+			return null;
+		}
+	}
 
 }
